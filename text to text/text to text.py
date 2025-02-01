78dfd8b5c98a81e30b8b21496023e299fbb741fa
SANJEEV P <sanjeev192005@gmail.com>
2025-02-01 16:28:11 +0530

    Add files via upload

---
+++ text to text/text to text.py
@@ -0,0 +1,21 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+#input
+prompt = "amypo technology is providing industrial training for"
+#encode
+input_ids=tokenizer.encode(prompt,return_tensors='pt')
+#generate
+output= model.generate(
+    input_ids,
+    max_length=100,
+    num_return_sequences=2,
+    do_sample=True,
+   #top_k=50,
+    temperature=0.7
+)
+print("generated_text:")
+for i,sequence in enumerate(output):
+   print(f"sequcence:{i+1}")
+   generated_text=tokenizer.decode(sequence,skip_special_tokens=True)
+   print(generated_text)
